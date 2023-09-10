0d013f6660ca1fc7eef6feebbd07f68431b68c8f
AradGolbaghi <arad6432@gmail.com>
2023-09-10 19:01:34 +0100

    getters and setters

--- Main.java
+++ Main.java
@@ -1,15 +1,20 @@
 public class Main {
+
     public static void main(String[] args) {
-
-        Car s = new Car("Ferrari", 4_000_000, 2023, "Orange");
-
+        Car s = new Car("Ferrari", 6_000_000, 2023, "Purple");
         Car r = new Car("Bugatti", 5_000_000, 2023, "Purple");
 
+        s.setColor("Black");
+        r.setColor("Black");
 
+        s.setPrice(s.getPrice() / 2);
+        r.setPrice(r.getPrice() / 2);
 
-        System.out.println("This " + s.make + " is worth $" + s.price + 
-        ". It was built in " + s.year + ". It is " + s.color + ".\n");
-        System.out.println("This " + r.make + " is worth $" + r.price + 
-        ". It was built in " + r.year + ". It is " + r.color + ".\n");
+        System.out.println("This " + s.getMake() + " is worth $" + s.getPrice() +
+                ". It was built in " + s.getYear() + ". It is " + s.getColor() + ".\n");
+        System.out.println("This " + r.getMake() + " is worth $" + r.getPrice() +
+                ". It was built in " + r.getYear() + ". It is " + r.getColor() + ".\n");
+
     }
+
 }
