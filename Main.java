13eae1623402b5a3d22dcd1f5808b2c4e6c8282a
AradGolbaghi <arad6432@gmail.com>
2023-09-11 20:40:11 +0100

    Airplain

--- Main.java
+++ Main.java
@@ -1,20 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
-        Car s = new Car("Ferrari", 6_000_000, 2023, "Purple");
-        Car r = new Car("Bugatti", 5_000_000, 2023, "Purple");
+        Person person = new Person("Rayan Slim", "Canadian", "01/01/1111", "Rayan Slim Canadian  01/01/1111", 5);
 
-        s.setColor("Black");
-        r.setColor("Black");
-
-        s.setPrice(s.getPrice() / 2);
-        r.setPrice(r.getPrice() / 2);
-
-        System.out.println("This " + s.getMake() + " is worth $" + s.getPrice() +
-                ". It was built in " + s.getYear() + ". It is " + s.getColor() + ".\n");
-        System.out.println("This " + r.getMake() + " is worth $" + r.getPrice() +
-                ". It was built in " + r.getYear() + ". It is " + r.getColor() + ".\n");
+        person.setSeatNumber(10);
+        System.out.println("Name: " + person.getName() + "\n" +
+                "Nationality: " + person.getNationality() + "\n" +
+                "Date of Birth: " + person.getDateOfBirth() + "\n" +
+                "Passport: " + person.getPassport() + "\n" + // Added + operator here
+                "Seat Number: " + person.getSeatNumber() + "\n");
 
     }
-
+    
 }
